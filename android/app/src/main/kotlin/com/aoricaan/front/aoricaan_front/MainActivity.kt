f1e3fbdc5c731146d46a413180204d3e1269d3ba
Carlos Añorve <carlosaarivera23@gmail.com>
2022-12-12 12:16:19 -0600

    first commit

---
+++ android/app/src/main/kotlin/com/aoricaan/front/aoricaan_front/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.aoricaan.front.aoricaan_front
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:rive/rive.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(title: 'Material App', home: Home());
+  }
+}
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Material App Bar'),
+      ),
+      body: Center(
+        child: RiveAnimation.asset(
+            "assets/fire_button.riv"
+
+
+        ),
+      ),
+    );
+  }
+}
